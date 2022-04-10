3da71296bfc4d5b563225b28a80579f0ee23ee24
Kehinde H. Fasunle <kfasunle@gmail.com>
2022-04-10 07:53:01 -0700

    Hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,10 @@
 
 /*
  * main - Entry point
- * 
  * Always return 0 (success)
- *
  * */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
