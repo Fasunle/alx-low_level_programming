2b4081cbfea0ae8ec160cb0d16d80aed73b81d7a
Kehinde H. Fasunle <kfasunle@gmail.com>
2022-04-10 07:47:36 -0700

    Hot fix: explicit return 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+
+/*
+ * main - Entry point
+ * 
+ * Always return 0 (success)
+ *
+ * */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
-	0;
+puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
