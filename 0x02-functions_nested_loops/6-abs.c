838aaedd9dc41b541d7b7704a91f798581a73a4d
Kehinde H. Fasunle <kfasunle@gmail.com>
2022-04-12 13:27:08 -0700

    Task 6: absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - prints modulus
+ * @c: accepts integer
+ * Description: return modulus
+ * Return: returns modulus
+ */
+int _abs(int c)
+i{
+	return (abs(c));
+}
