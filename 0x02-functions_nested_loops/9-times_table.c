bf03cce21c3306774bfcdaa5de256a9745d3aa63
Kehinde H. Fasunle <kfasunle@gmail.com>
2022-04-13 01:10:44 -0700

    Task 9: 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - multiples of 9
+ * @void: no argument
+ * Description: Multiply 9 with 1 through 12
+ * Return: returns void
+ */
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+			printf("%d, ", k);
+		}
+		printf("\n");
+	}
+}
