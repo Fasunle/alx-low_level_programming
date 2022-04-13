cc0eae0ed2e9427ca5a978e64907574e904f3ef0
Kehinde H. Fasunle <kfasunle@gmail.com>
2022-04-13 01:47:03 -0700

    Task 10: Addition function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers together
+ * @a: an integer
+ * @b: an integer
+ * Description: Addition function
+ * Return: result of addition of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+}
