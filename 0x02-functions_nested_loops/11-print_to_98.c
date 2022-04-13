cee8a1546b897627ae0e95004fec7be0874f893a
Kehinde H. Fasunle <kfasunle@gmail.com>
2022-04-13 02:37:50 -0700

    Hot fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,31 +13,37 @@ void print_to_98(int n)
 	{
 		printf("%d", n);
 	}
-
-	while (n >= 98)
+	else if (n > 98)
 	{
-		if (n == 98)
+		while (n >= 98)
 		{
-			printf("%d", n);
+			if (n == 98)
+			{
+				printf("%d", n);
+			}
+			else if (n > 98)
+			{
+				printf("%d, ", n);
+			}
+			n--;
 		}
-		else
-		{
-			printf("%d, ", n);
-		}
-		n--;
 	}
 
-	while (n <= 98)
+	else if (n < 98)
 	{
-		if (n == 98)
+
+		while (n <= 98)
 		{
-			printf("%d", n);
+			if (n == 98)
+			{
+				printf("%d", n);
+			}
+			else if (n < 98)
+			{
+				printf("%d, ", n);
+			}
+			n++;
 		}
-		else
-		{
-			printf("%d, ", n);
-		}
-		n++;
 	}
 	printf("\n");
 }
