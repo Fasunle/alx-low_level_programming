49a98f4082191ebabd94f4c8c24848975f363f89
Kehinde H. Fasunle <kfasunle@gmail.com>
2022-04-13 03:14:22 -0700

    Advanced task: 100 times_table printer

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table - multiplication table
+ * @n: takes a number
+ * Description: Create multiplication table
+ * Return: returns void
+ */
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			k = i * j;
+			if (k < 10)
+			{
+				printf("   ");
+			}
+			else if (k <= 99)
+			{
+				printf("  ");
+			}
+			else if (k <= 999)
+			{
+				printf(" ");
+			}
+
+			if (j != n)
+			{
+				printf("%d,", k);
+			}
+			else
+			{
+				printf("%d", k);
+			}
+		}
+		printf("\n");
+	}
+}
