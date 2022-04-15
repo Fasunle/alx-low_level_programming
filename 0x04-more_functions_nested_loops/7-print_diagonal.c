7f280335ae1981307ee2c93967548c4dceba5caf
Kehinde H. Fasunle <kfasunle@gmail.com>
2022-04-15 15:15:12 -0700

    print diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints on the diagonal
+ * @n: integer
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(' ');
+	}
+	_putchar(92);
+	_putchar('\n');
+}
