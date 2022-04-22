dbf17aa44f1424a17a68dd2ef5dd8ad4b311b391
Kehinde H. Fasunle <kfasunle@gmail.com>
2022-04-21 18:54:45 -0700

    Check string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - prints string length
+ * @s: pointer to string
+ * Return: 0 for success or 1 for failure
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	return (i);
+}
